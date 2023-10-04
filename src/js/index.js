c41cdac23d462004fbbb703b41f7b93dc8f28c67
Gabriel Porto <56123318+GabsPorto@users.noreply.github.com>
2023-10-04 03:11:36 -0400

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -13,7 +13,7 @@
 */
 
 const botoes = document.querySelectorAll(".botao");
-const personagens = document.querySelectorAll(".personagem")
+const personagens = document.querySelectorAll(".personagem");
 
 botoes.forEach((botao, indice) => {
     botao.addEventListener("click", () => {
